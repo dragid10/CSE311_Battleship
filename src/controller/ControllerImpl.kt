759d85da57c6c16cb31c6a02c7d93f1401986ac6
Alex Oladele <Dragid10@gmail.com>
2018-02-11 14:41:28 -0500

    Start code for future implementation

--- src/controller/ControllerImpl.kt
+++ src/controller/ControllerImpl.kt
@@ -3,11 +3,37 @@ package controller
 import view.BattleshipView
 
 class ControllerImpl(private val view: BattleshipView) : Controller {
+    //    Member Variables
+    lateinit var player1: Player
+    lateinit var player2: Player
+
+    //    Constructor
     init {
         view.setController(this)
     }
 
+    //    Overridden Functions
     override fun hitCoordinate(coordinates: String) {
 
+    }
+
+    //    Member Functions
+
+    /**
+     * This method initializes the game, by randomizing player's ship locations, and setting up the game board
+     * @param
+     * @return Void
+     */
+    fun setupGame() {
+//        Create Players 1 & 2
+        createPlayers()
+
+//        Create gameboard
+        TODO("Do Swing GUI stuff here eventually")
+    }
+
+    private fun createPlayers() {
+        player1 = Player()
+        player2 = Player()
     }
 }
