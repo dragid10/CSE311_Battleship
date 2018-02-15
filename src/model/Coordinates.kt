35996790bd6ace3963a709900cfa23eece00fa8e
MintonJI <36247290+MintonJI@users.noreply.github.com>
2018-02-15 11:28:35 -0500

    Added a Coordinates data type class; it'll be useful for the Hash Maps' keys.

---
+++ src/model/Coordinates.kt
@@ -0,0 +1,8 @@
+package model
+
+/**
+ * This class is used to define a Coordinates data type. It will be useful
+ * for the Grids' and Ships' Hash Map keys.
+ */
+class Coordinates(var x: Int, var y: Int) {
+}
