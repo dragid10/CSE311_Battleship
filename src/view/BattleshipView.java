87435fe52da17c66fe20398cd30eb5d6d2e63b43
adrianajohnson <31991683+adrianajohnson@users.noreply.github.com>
2018-02-16 16:06:42 -0500

    Add files via upload

---
+++ src/view/BattleshipView.java
@@ -0,0 +1,76 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public interface BattleshipView {
+
+	/**
+	 * Display hit on ship
+	 * @param x
+	 * @param y
+	 */
+	public void updateBoardAsHit(int x, int y);
+	
+	/**
+	 * Display miss fire in water
+	 * @param x
+	 * @param y
+	 */
+	public void updateBoardAsMiss(int x, int y);
+	
+	/**
+	 * Display a sunken ship
+	 * @param ship
+	 */
+	//public void displaySunkenShip(Ship ship);
+
+	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		//Custom button text
+		Object[] options = {"GUI View",
+		                    "Text View"};
+		int n = JOptionPane.showOptionDialog(frame,
+		    "What view would you like to play Battleship in?",
+		    "View Selection",
+		    JOptionPane.YES_NO_CANCEL_OPTION,
+		    JOptionPane.QUESTION_MESSAGE,
+		    null,
+		    options,
+		    options[0]);
+
+		
+		
+		/*Object[] possibleValues = { "First", "Second", "Third" };
+		Object selectedValue = JOptionPane.showInputDialog(null,
+		"Choose one", "Input",
+		JOptionPane.INFORMATION_MESSAGE, null,
+		possibleValues, possibleValues[0]);
+		Object[] options = {"GUI View", "Text View"};
+		JOptionPane pane = new JOptionPane(frame,
+			    JOptionPane.YES_NO_CANCEL_OPTION,
+			    JOptionPane.QUESTION_MESSAGE,
+			    null,
+			    options,
+			    options[0]);
+		
+	     Object selectedValue = pane.getValue();*/
+		
+		if(n == 0) {
+			BattleshipViewGUI gui = new BattleshipViewGUI();
+			gui.setTitle("Battleship");
+			gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			/*JFrame frame = new JFrame();
+        	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	frame.getContentPane().add(gui);
+        	frame.setTitle("Battleship");
+        	frame.setResizable(false);
+        	//frame.add(gui);
+        	frame.pack();
+        	frame.setVisible(true);*/
+		}else {
+			System.out.println("You chose the text view");
+			BattleshipViewText text = new BattleshipViewText(10,10);
+			
+		}
+		
+	}
+}
---
+++ src/view/BattleshipViewText.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class BattleshipViewText implements BattleshipView{
+    Scanner input = new Scanner(System.in);
+    public static final boolean DEBUG = false;
+    String[][] board;
+    
+	public BattleshipViewText(int numRows, int numCols) {
+		 board = new String[numRows][numCols];
+         createBoard(board);
+         showBoard(board);
+	}
+
+	
+
+    public static void breakln(){
+       System.out.println("_____________________________________");
+       System.out.println("");
+    }
+    
+    public static void createBoard(String[][] board)
+    {
+       for(int r = 0; r < board.length; r++)
+       {
+          for(int c = 0; c < board[0].length; c++)
+          {
+             board[r][c] = "-";
+          }
+       }
+    }
+    public static void showBoard(String[][] board)
+    {
+       breakln();
+       for(int r = 0; r < board.length; r++)
+       {
+          if(DEBUG == true)
+          {
+             for(int c = 0; c < board[0].length; c++)
+             {
+                System.out.print(" "+board[r][c]);
+             }
+             System.out.println("");
+          }
+          else
+          {
+             for(int c = 0; c < board[0].length; c++)
+             {
+                if(board[r][c].equals("S"))
+                {
+                   System.out.print(" "+"-");
+                }
+                else
+                {
+                   System.out.print(" "+board[r][c]);
+                }
+             }
+             System.out.println("");
+          }
+       }
+       breakln();
+    }
+    
+	
+
+	@Override
+	public void updateBoardAsHit(int x, int y) {
+		// TODO Auto-generated method stub
+		board[x][y] = "*";
+		
+	}
+
+	@Override
+	public void updateBoardAsMiss(int x, int y) {
+		// TODO Auto-generated method stub
+		board[x][y] = "M";
+		
+	}
+}
