b0b58f91fe7f00f5f46174454fae319caf6f5a24
fitchae <fitchae@miamioh.edu>
2018-02-16 19:02:50 -0500

    Start work on Controller

--- src/controller/ControllerImpl.kt
+++ src/controller/ControllerImpl.kt
@@ -1,12 +1,21 @@
 package controller
 
+import model.Coordinates
+import model.Game
+import model.Grid
 import model.Player
 import view.TicTacToeView
 
 class ControllerImpl(private val view: TicTacToeView) : Controller {
     // ========================= Member Variables =========================
     lateinit var player1: Player
-    lateinit var player2: Player
+    //val = immutable
+    //var = mutable
+    val grid = Grid()
+    val game = Game()
+    lateinit var coordinates: Coordinates
+
+
 
     // ========================= Overridden Functions =========================
     override fun startGame() {
@@ -14,12 +23,18 @@ class ControllerImpl(private val view: TicTacToeView) : Controller {
     }
 
 
-    override fun takeSpot(coordinate: String) {
-
+    override fun takeSpot(row: Int, column: Int ) {
+        coordinates = Coordinates(row,column)
+        
     }
 
     // ========================= Member Functions =========================
     private fun playGame() {
+        //user asked for move
+        //Ask view for user input
+        //view take input
+
+        takeSpot()
 
     }
 }
