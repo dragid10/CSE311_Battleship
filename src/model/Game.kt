9009696c87b7ca575052767c5eb2004f4aa502c1
MintonJI <36247290+MintonJI@users.noreply.github.com>
2018-02-17 22:36:58 -0500

    Added grid obj to game and part of the gameplay implementation

--- src/model/Game.kt
+++ src/model/Game.kt
@@ -11,7 +11,19 @@ class Game {
 
     var turnCount = 1;
 
+    var gameGrid : Grid = Grid()
+
     fun main(args: Array<String>) {
+
+        if(turnCount % 2 == 1) {
+            println("Player X's turn.")
+        }
+
+        else{
+            println("Player O's turn.")
+        }
+
+        print("Please input your move: ")
 
         with(Scanner(System.`in`)) {
             val x = nextInt()
