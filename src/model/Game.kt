099afeac2cbe522496c4f26ba94e28ebc6b0ab32
MintonJI <36247290+MintonJI@users.noreply.github.com>
2018-02-19 17:41:50 -0500

    > Commented the entirety of Game. > Changed the secondary constructor in Grid to a primary constructor.

--- src/model/Game.kt
+++ src/model/Game.kt
@@ -8,19 +8,39 @@ class Game {
     val EMPTY_CELL = 0
     private val PLAYER_X = 1
     private val PLAYER_O = 2
-    var turnCount = 1
-    var gameGrid: Grid = Grid()
 
-    // Used for checking if a player has won
+    // Used for which player is currently making a move.
     var currPlayer = -1
+
+    // Determines if the game ended in a Draw (0) or if Player X (1) or Player O (2) won.
     var winner = 0
+
+    // Used to see if a player has won during their turn.
     var playerWon = false
+
+    // Used to see if the game has ended after a turn.
     var gameOver = false
 
+    // Used for determining which player goes next and if there is a tie.
+    var turnCount = 1
+
+    // Initializes the game's grid.
+    var gameGrid: Grid = Grid()
+
+    /**
+     * This function will loop until either player has won or the game results in a draw.
+     * First, the function will determine whose turn it is. Then, it asks the player to input
+     * their move's coordinates. If the coordinates are valid, the game will update the game's grid
+     * and will check to see if the player has won. If they have, the game ends. If they have not,
+     * the turn counter increments by one and the other player may take their turn. If after nine
+     * turns, neither player has won, the game ends in a draw.
+     */
     fun main(args: Array<String>) {
 
+        // While the game hasn't ended
         while (!gameOver) {
 
+            // Used for determining whose turn it is.
             if (turnCount % 2 == 1) {
                 println("Player X's turn.")
                 currPlayer = PLAYER_X
@@ -31,20 +51,28 @@ class Game {
 
             print("Please input your move: ")
 
+            // Takes user input and transforms it into the Coordinate data type.
             val input = Scanner(System.`in`)
             val x = input.nextInt()
             val y = input.nextInt()
             val lastMove = Coordinates(x, y)
 
+            // A move is only valid if the cell the coordinates are pointing to is empty.
             if (gameGrid.getCellStatus(lastMove) == EMPTY_CELL) {
+
+                // Gives cell's ownership to the current player.
                 gameGrid.setCellStatus(lastMove, currPlayer)
 
+                // Holds the Boolean determining if a player won.
                 playerWon = hasWon(currPlayer, lastMove)
 
                 if (playerWon) {
                     gameOver = true
                     winner = currPlayer
-                } else if (!playerWon && turnCount == 9) {
+                }
+
+                // Game resulted in a draw.
+                else if (!playerWon && turnCount == 9) {
                     gameOver = true
                     winner = 0
                 } else {
@@ -55,12 +83,22 @@ class Game {
         }
     }
 
-    fun hasWon(currPlayer: Int, lastMove: Coordinates): Boolean {
+    /**
+     * Uses the currently player's move to determine if they won during their turn.
+     * Checks the row and column the last move was made in and checks the two diagonals.
+     * If all of the cells in a row, column, diagonal, or reverse diagonal are owned
+     * by the player, then the player has won the game.     *
+     *
+     * @param currPlayer The player who is currently taking their turn.
+     * @param lastMove The move the current player just made.
+     * @return If the player has won the game.
+     */
+    private fun hasWon(currPlayer: Int, lastMove: Coordinates): Boolean {
 
         val row = lastMove.row
         val col = lastMove.col
 
-        // Check row win
+        // Checks row win.
         for (n in 1..3) {
             if (gameGrid.getCellStatus(Coordinates(n, col)) != currPlayer) {
                 break
@@ -69,7 +107,7 @@ class Game {
             }
         }
 
-        // Check col win
+        // Checks column win.
         for (n in 1..3) {
             if (gameGrid.getCellStatus(Coordinates(row, n)) != currPlayer) {
                 break
@@ -78,7 +116,7 @@ class Game {
             }
         }
 
-        // Check diagonal win
+        // Checks diagonal win.
         for (n in 1..3) {
             if (gameGrid.getCellStatus(Coordinates(n, n)) != currPlayer) {
                 break
@@ -87,7 +125,7 @@ class Game {
             }
         }
 
-        // Check reverse diagonal win
+        // Checks reverse diagonal win.
         for (n in 1..3) {
             if (gameGrid.getCellStatus(Coordinates(4 - n, n)) != currPlayer) {
                 break
@@ -96,7 +134,7 @@ class Game {
             }
         }
 
-        // If none of the win conditions return true. No one won. Return false.
+        // If none of the win conditions return true. The player didn't won.
         return false
     }
 
