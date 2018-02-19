594d2507971003ad5390f19c2aea5f03c93018e4
fitchae <fitchae@miamioh.edu>
2018-02-19 16:28:31 -0500

    Adds test cases

---
+++ src/model/CoordinatesTest.kt
@@ -0,0 +1,22 @@
+package model
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CoordinatesTest {
+
+    @org.junit.jupiter.api.Test
+    fun getRow() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setRow() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getCol() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setCol() {
+    }
+}
